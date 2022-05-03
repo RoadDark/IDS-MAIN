2416e8286b27eac246b575c67c5abcdfae7cf9d7
Callum Du Heaume <callumxdu@gmail.com>
2022-05-03 12:45:00 +0100

    IP Source Address analytics Working

--- IDS BASE/Form1.Designer.cs
+++ IDS BASE/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.RUN_btn = new System.Windows.Forms.Button();
+            this.Analyse_btn = new System.Windows.Forms.Button();
+            this.sensitivity_txt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // RUN_btn
@@ -41,20 +43,43 @@
             this.RUN_btn.UseVisualStyleBackColor = true;
             this.RUN_btn.Click += new System.EventHandler(this.RUN_btn_Click);
             // 
+            // Analyse_btn
+            // 
+            this.Analyse_btn.Location = new System.Drawing.Point(158, 12);
+            this.Analyse_btn.Name = "Analyse_btn";
+            this.Analyse_btn.Size = new System.Drawing.Size(140, 81);
+            this.Analyse_btn.TabIndex = 1;
+            this.Analyse_btn.Text = "Analyse";
+            this.Analyse_btn.UseVisualStyleBackColor = true;
+            this.Analyse_btn.Click += new System.EventHandler(this.Analyse_btn_Click);
+            // 
+            // sensitivity_txt
+            // 
+            this.sensitivity_txt.Location = new System.Drawing.Point(304, 12);
+            this.sensitivity_txt.Name = "sensitivity_txt";
+            this.sensitivity_txt.PlaceholderText = "Sensitivity";
+            this.sensitivity_txt.Size = new System.Drawing.Size(119, 23);
+            this.sensitivity_txt.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 551);
+            this.Controls.Add(this.sensitivity_txt);
+            this.Controls.Add(this.Analyse_btn);
             this.Controls.Add(this.RUN_btn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button RUN_btn;
+        private Button Analyse_btn;
+        private TextBox sensitivity_txt;
     }
 }
--- IDS BASE/Form1.cs
+++ IDS BASE/Form1.cs
@@ -21,13 +21,103 @@ namespace IDS_BASE
         private void RUN_btn_Click(object sender, EventArgs e)
         {
 
-            // Hard Coded for TESTING need to may user changable
+            // Hard Coded for TESTING need to make user changable
              Input(@"H:\Visual Studio Solutions\IDS BASE\IDS BASE\bin\Debug\DATA\MixedBefireInfection.xlsx");
-           
+        }
+        private void Analyse_btn_Click(object sender, EventArgs e)
+        {
 
-        }      
 
-        public void Input(string filePath)
+            double[] safetyRating = Analyse(@"H:\Visual Studio Solutions\IDS BASE\IDS BASE\bin\Debug\DATA\MixedBefireInfection.xlsx");
+
+        }
+        public double[] Analyse(string dataSet)
+        {
+            double sensitivity;
+            try
+            {
+                 sensitivity = Convert.ToDouble(sensitivity_txt.Text);
+            }
+            catch
+            {
+                 MessageBox.Show("USE Numbers in the sensitivity bettween 1 and 0 (0.3)");
+                 sensitivity = .5;
+            }
+
+            double[] safety_Array = new double[6];
+            // Creates the EXCEL Interop for use
+            Excel.Application? excel = null;
+            Excel.Workbook? wkb = null;
+            Excel.Range? range = null;
+            Excel.Worksheet? sheet = null;
+            // starts the EXCEL Interop
+            excel = new Excel.Application();
+            // Opens the workbook File
+            wkb = excel.Workbooks.Open(dataSet);
+            //opens the sheet *Need to make the name changable / open sheet [1] by default*
+            sheet = wkb.Sheets["MixedBefireInfection"];
+            // gets the used number of used cells
+            range = sheet.UsedRange;
+            // count of used rows and coloums
+            int rowCount = range.Rows.Count;
+            int ColoumnsCount = range.Columns.Count;
+            //itrate thew every single ROW for a SELECTED Coloum            
+            int n = 500;
+            string[] data = new string[6];
+            for (int i = 8000; i <= 16000; i++) // using the Seccind set of 8000 ROWS for this PART 
+            {
+
+                for (int j = 4; j < 5; j++) // Goto 9 For all relavent Data int the END GOAL
+                {
+                       // check if cell has DATA then read it
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Text != null)
+                    {
+
+                        data[j-4] = (range.Cells[i, j].Text);                      
+
+                    }
+                }
+
+
+                // Run Through Each Part of the Data Packet againts its Trained NODE
+                double safety = SourceAddressNodeAnalyse(src_G,srcCommonality_G,data[0]);
+                safety_Array[0] = safety;
+
+
+                double saftyRating=0;
+                foreach(double commanality in safety_Array)
+                {
+                    saftyRating = saftyRating + commanality;
+                    
+                }
+                saftyRating = saftyRating / 1; // Will need to be 6 by the END
+                if(saftyRating < sensitivity)
+                {
+                    Console.WriteLine("Packet: " + i  + " Has Been Flagged With Rating of: " + saftyRating.ToString());
+                }
+
+            }
+            //clean excel process
+            Console.WriteLine("Done.. Starting clean up");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(range);
+            Marshal.ReleaseComObject(sheet);
+
+            //close and release
+            wkb.Close();
+            Marshal.ReleaseComObject(wkb);
+
+            //quit and release
+            excel.Quit();
+            Marshal.ReleaseComObject(excel);
+            return safety_Array;
+        }
+
+
+         public void Input(string filePath)
         {// Method for Reading the EXCEL File and providing in to the Next NODE's in the programme
             Console.WriteLine(filePath);
 
@@ -48,13 +138,13 @@ namespace IDS_BASE
             range = sheet.UsedRange;
 
             string[] Results = scrape(4);
-            (src_G, srcCommonality_G) = SourceAddressNode(Results, true);
+            (src_G, srcCommonality_G) = SourceAddressNodeTraining(Results);
 
             // used to Scrape the Rows out of a single coloum !!! CAPED AT 15000 rows for testing because of SPEED !!!
             string[] scrape(int coloum) {
                 // count of used rows 
                 int rowCount = range.Rows.Count;
-                ; // Selets the coloum to Scrape
+                 // Selets the coloum to Scrape
                                 //itrate thew every single ROW for a SELECTED Coloum            
                 int n = 500;
                 List<string> Scrape = new List<string> { };
@@ -105,9 +195,9 @@ namespace IDS_BASE
             
 
         }
-        public (string[] , double[]) SourceAddressNode(string[] Sources, bool Trainng)
-        {if (Trainng)
-            {   
+        public (string[] , double[]) SourceAddressNodeTraining(string[] Sources)
+        { 
+              
                 List<string> Src = new List<string>();
                 List<int> comanality = new List<int>();
                 string Sample = string.Empty;
@@ -142,19 +232,21 @@ namespace IDS_BASE
                 for (int i = 0; i < comanality_A.Length; i++){
                     comanality_D[i] = (double)comanality_A[i] / (double)comanality_A.Max();                                   
                 }
-
-
-
-
-
                 return (Src_A, comanality_D);
 
-         }
-         else
-         {
-                return (null, null);
-         }
-
+        }
+        public double SourceAddressNodeAnalyse(string[]?sources,double[]? srcCommonality,string data)
+        {
+            for (int i = 0; i < sources.Length; i++) 
+            { 
+               if (data == sources[i])
+                {
+                    double commonality = srcCommonality[i];
+                    return commonality;
+                }   
+               
+            }                               
+            return 0;
         }
 
 
